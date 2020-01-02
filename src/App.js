6180b375adcd2a54cc4ee0333ae9a8fcd514ebca
sturrd <54156889+sturrd@users.noreply.github.com>
2020-01-02 23:34:05 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import HomeComponent from "./component/Home";
+
+function App() {
+  return (
+    <>
+      <HomeComponent />
+    </>
+  );
+}
+
+export default App;
